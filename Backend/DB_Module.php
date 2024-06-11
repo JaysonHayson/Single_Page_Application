3f1f683b5b9f24a1143f96c943ab43c18354c0a2
Dramatocles <172032003+Dramatocles@users.noreply.github.com>
2024-06-11 07:24:15 +0200

    Error Handling + DB conditional Select

--- Backend/DB_Module.php
+++ Backend/DB_Module.php
@@ -2,8 +2,51 @@
     
     require './config.php';
 
-    $pdo = new PDO("mysql:host=$DBHost;dbname=$DB_DBName", DBUSERNAME, DBPASSWORD);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    printf($pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS));
+    try {
+        $pdo = new PDO("mysql:host=$DBHost;dbname=$DB_DBName", DBUSERNAME, DBPASSWORD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $ConnStat = $pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+        if($ConnStat != 'Localhost via UNIX socket'){throw new Exception("$ConnStat");}
+        
+        
+        // Example Call
+        /*     
+        print "<br>PDO:<br>";
+        $condition[] = ["UID" => "1"];
+        $results[] = selectWithWhereCondition($pdo,$DBTableName1,$condition);
+        print "<pre>";
+        print_r($results);
+        print "</pre>";
+        */
+    } catch (PDOException $e) {
+        echo 'Error: ' . $e->getMessage();
+    } catch (Exception $e){
+        echo 'Error: Unexpected result:' . $e->getMessage();
+    }
 
+
+    function selectWithWhereCondition($pdo,$table, $condition) {
+        if (empty($table)) {
+            throw new Exception("Table name cannot be empty.");
+        }
+
+        if (empty($condition) || !is_array($condition)) {
+            throw new Exception("Condition must be a non-empty associative array.");
+        }
+
+        $whereClause = "";
+        foreach($condition as $Arr){
+            foreach ($Arr as $key => $value) {
+                $whereClause = "`$key` = '$value'";
+            }
+        }
+        // Constructing the SQL query
+        $sql = "SELECT * FROM $table WHERE $whereClause";
+        // Preparing and executing the statement
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        // Fetching all rows of the results
+        return $stmt->fetchall(PDO::FETCH_ASSOC);
+    }
 ?>
