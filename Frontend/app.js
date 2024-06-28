b795e07d57cbacc95470766eeed101d9501d299a
CprN <cprfatih@gmail.com>
2024-06-28 10:18:52 +0200

    test1

--- Frontend/app.js
+++ Frontend/app.js
@@ -651,13 +651,10 @@ function createCheckout() {
       checkout.appendChild(itemDiv);
     });
 
-    const totalContainer = createElement(
-      "div"
-    );
+    const totalContainer = document.createElement("div");
     totalContainer.innerHTML =`
-    <p>${total}</p>`;
+    <p>Gesamtpreis: ${totalPrice.toFixed(2)}€}</p>`;
     checkout.appendChild(totalContainer);
-    checkout.textContent = total.toFixed(2);
     
   }
   return checkout;
