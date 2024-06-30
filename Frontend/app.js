fd0fbc1f2eacc4674d782ffe756ffd81ff9550a1
Earlo18 <137812950+Earlo18@users.noreply.github.com>
2024-06-30 23:26:05 +0200

    Insert adress into DB

--- Frontend/app.js
+++ Frontend/app.js
@@ -494,6 +494,7 @@ function handleRegister(event) {
   const username = document.getElementById("userNameInput").value;
   const email = document.getElementById("emailInput").value;
   const pw = document.getElementById("passwordInput").value;
+  const adress = document.getElementById("adressInput").value;
 
   fetch("../Backend/index.php", {
     method: "POST",
@@ -501,6 +502,7 @@ function handleRegister(event) {
       Command: "registerNewUser",
       userFirstName: firstName,
       userLastName: lastName,
+      userAdress: adress,
       userName: username,
       userEmail: email,
       userPW: pw,
