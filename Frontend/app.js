c9b2210af06bf0a27b3c9c05802f2c69d94ac688
JaysonHayson <jesse.owen.wi@gmail.com>
2024-07-02 07:11:13 +0200

    getting there

--- Frontend/app.js
+++ Frontend/app.js
@@ -833,7 +833,9 @@ function createOrderConfirmation(userData, cartSummary) {
       clonedOrderForm.style.backgroundColor = "white";
       clonedOrderForm.style.color = "black";
       clonedOrderForm.style.padding = "20px";
-      clonedOrderForm.style.width = "100%";
+      clonedOrderForm.style.width = "595px"; // A4 width in pixels
+      clonedOrderForm.style.maxWidth = "100%";
+      clonedOrderForm.style.boxSizing = "border-box";
 
       // Append the cloned element to the body
       document.body.appendChild(clonedOrderForm);
